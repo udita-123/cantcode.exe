a81e5640970d8a1ed8a8042a8bdf9ffc99f72956
udita-123 <uditas.305@gmail.com>
2025-08-24 16:07:31 +0530

    Delete save_classes.py

--- save_classes.py
+++
@@ -1,9 +0,0 @@
-import json
-
-classes = ["bread", "curry", "fruits", "other", "packaged", "rice"]  # update with your folders
-
-with open("classes.json", "w") as f:
-    json.dump(classes, f)
-
-print("✅ Saved classes.json")
-
